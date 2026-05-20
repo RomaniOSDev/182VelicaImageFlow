7ad597d850d9d11f2c6fb81111ef3e944a8b01d8
Roman <romazaniosdev@gmail.com>
2026-05-20 22:46:54 +0300

    Initial Commit

---
+++ 182VelicaImageFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  182VelicaImageFlow
+//
+//  Created by Roman on 5/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
